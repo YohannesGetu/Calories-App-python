e31c753cb31f820520240df873b7dccad09924cf
Yohannes Getu <nede.evoli@gmail.com>
2021-08-25 15:11:43 +0300

    added the empty class and initial setup files

---
+++ calorie.py
@@ -0,0 +1,13 @@
+class Calorie:
+    """Represent amount of calories calculated with
+    BMR = 10*weight + 6.25*height - 5*age - 10*temperature
+    """
+
+    def __init__(self, weight, height, age, temperature):
+        self.weight = weight
+        self.height = height
+        self.age = age
+        self.temperature = temperature
+
+    def calculate(self):
+        pass
