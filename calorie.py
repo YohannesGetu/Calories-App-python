6680d452388471032fb1742609899d377e648cc1
Yohannes Getu <nede.evoli@gmail.com>
2021-08-25 15:42:07 +0300

    implemented the calorie class

--- calorie.py
+++ calorie.py
@@ -1,3 +1,6 @@
+from temperature import Temperature
+
+
 class Calorie:
     """Represent amount of calories calculated with
     BMR = 10*weight + 6.25*height - 5*age - 10*temperature
@@ -10,4 +13,10 @@ class Calorie:
         self.temperature = temperature
 
     def calculate(self):
-        pass
+        result = 10 * self.weight + 6.5 * self.height + 5 - self.temperature * 10
+
+
+if __name__ == "__main__":
+    temperature = Temperature(country="ethiopia", city="addis-ababa").get()
+    calorie = Calorie(weight=70, height=175, age=32, temperature=temperature)
+    print(calorie.calculate())
